c777f3f37c16cf8e7de81f207539afb807e0a292
James Lee <l1026112586@163.com>
2020-06-10 08:58:14 +0800

    增加proxy的备注

--- my_app_spiders/豆果美食爬虫代码/handle_proxy.py
+++ my_app_spiders/豆果美食爬虫代码/handle_proxy.py
@@ -2,6 +2,7 @@ import requests
 
 #114.246.2.155"
 url = 'http://ip.hahado.cn/ip'
+#{'http':'http://用户名:密码@ip代理服务器地址:端口号'}
 proxy = {'http':'http://H211EATS9O5745KC:F8FFBC929EB7D5A7@http-cla.abuyun.com:9030'}
 response = requests.get(url=url,proxies=proxy)
 print(response.text)
